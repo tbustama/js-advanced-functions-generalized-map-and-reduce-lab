5121be807c364cb99f13512704f94db04d2f5f36
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-20 11:36:36 -0800

    commit tree e869dee2c9cc8738ddefb2d41a662c65b8b8d52e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+function map(arr, fun) {
+  return arr.map(fun);
+}
+
+function reduce(arr, start = null) {
+  if (start) {
+    let temp = arr.reduce(start);
+    return temp(start) + start;
+  } else {
+    return arr.reduce(start);
+  }
+}
